87aaf0949616d6ac04ff80dcc8571624a5cc79a1
YasserABoutarf <Yasser.Boutarf@wolfandco.com>
2025-11-04 15:14:32 -0500

    Add check for installer file existence

--- Tvalue.ps1
+++ Tvalue.ps1
@@ -1,6 +1,6 @@
 $installerPath = "F:\APPS\TValue 6\TValue6-02-Setup.exe"
 
-# Check if the file exists
+
 if (Test-Path $installerPath) {
     Write-Host "Installer found. Running setup..."
     Start-Process -FilePath $installerPath -Wait
