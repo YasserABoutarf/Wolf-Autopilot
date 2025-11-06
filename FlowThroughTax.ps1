eac688203955d51f11f2a2e3d6bf5ea0168de0ae
YasserABoutarf <Yasser.Boutarf@wolfandco.com>
2025-11-06 13:57:35 -0500

    Update installer paths for Caseware 2024

--- FlowThroughTax.ps1
+++ FlowThroughTax.ps1
@@ -1,8 +1,10 @@
 $installerPath1 = "F:\APPS\TValue 6\TValue6-02-Setup.exe"
-$installerPath2 = "F:\APPS\CaseWare\Caseware 2023\ConnectorSetup.exe"
-$installerPath3 = "F:\APPS\CaseWare\Caseware 2023\WP2023USSYNC_630DD56E181047A0AD15_.exe"
-$installerPath4 = "\\bos-ichannel\ichannel\web\ConarciFetch\Prerequisites\windowsdesktop-runtime-8.0.5-win-x64.exe"
-$installerPath5 = "\\bos-ichannel\ichannel\web\ConarciFetch\Setup\ConarciFetchInstaller.exe"
+$installerPath2 = "C:\Program Files\Backup\Caseware 2024 Files\ConnectorSetup.exe"
+$installerPath3 = "C:\Program Files\Backup\Caseware 2024 Files\WP2024USSYNC_EF4E1C8024CAF2620E0F_.exe"
+$installerPath4 = "C:\Program Files\Backup\Caseware 2024 Files\CW2024TaxUpdate.exe"
+$installerPath5 = "\\bos-ichannel\ichannel\web\ConarciFetch\Prerequisites\windowsdesktop-runtime-8.0.5-win-x64.exe"
+$installerPath6 = "\\bos-ichannel\ichannel\web\ConarciFetch\Setup\ConarciFetchInstaller.exe"
+
 
 # Tvalue Install
 If(Test-Path $installerPath1) {
@@ -30,19 +32,28 @@ If(Test-Path $installerPath3) {
     Write-Host "Installer Failed"
 }
 
-#Ichannel prereqs
+#Tax update
 If(Test-Path $installerPath4) {
-    Write-Host "Downloading Ichannel prereqs...."
+    Write-Host "Downloading Caseware...."
     Start-Process -FilePath $installerPath4 -Wait
     Write-Host "Setup Complete"
 } else {
     Write-Host "Installer Failed"
 }
 
-#ichannel
+#Ichannel prereqs
 If(Test-Path $installerPath5) {
-    Write-Host "Downloading Ichannel..."
+    Write-Host "Downloading Ichannel prereqs...."
     Start-Process -FilePath $installerPath5 -Wait
+    Write-Host "Setup Complete"
+} else {
+    Write-Host "Installer Failed"
+}
+
+#ichannel
+If(Test-Path $installerPath6) {
+    Write-Host "Downloading Ichannel..."
+    Start-Process -FilePath $installerPath6 -Wait
     Write-Host "Setup finished"
 } else {
     Write-Host "Installer Failed"
