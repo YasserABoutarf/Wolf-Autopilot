34f172c32fe386cf89749ea6e38b0e0fdde9e3e8
YasserABoutarf <Yasser.Boutarf@wolfandco.com>
2025-11-06 11:28:47 -0500

    Fix installer path variable in FlowThroughTax.ps1

--- FlowThroughTax.ps1
+++ FlowThroughTax.ps1
@@ -1,9 +1,9 @@
 $installerPath = "F:\APPS\TValue 6\TValue6-02-Setup.exe"
 
 # Tvalue Install
-If(Test-Path $installerPath1) {
+If(Test-Path $installerPath) {
     Write-Host "T Value Found. Downloading..."
-    Start-Process -FilePath $installerPath1 -Wait
+    Start-Process -FilePath $installerPath -Wait
     Write-Host "Setup Complete"
 } else {
     Write-Host "Installer failed."
