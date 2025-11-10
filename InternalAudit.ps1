a880d74cd5c7c8d89a97a45ed96a86c6f2a1af2e
YasserABoutarf <Yasser.Boutarf@wolfandco.com>
2025-11-09 19:47:16 -0500

    Add download logic for TValue and ichannel installers

--- InternalAudit.ps1
+++ InternalAudit.ps1
@@ -1,1 +1,31 @@
+$tvalue = "F:\APPS\TValue 6\TValue6-02-Setup.exe"
+$ichannelprereq = "\\bos-ichannel\ichannel\web\ConarciFetch\Prerequisites\windowsdesktop-runtime-8.0.5-win-x64.exe"
+$ichannel = "\\bos-ichannel\ichannel\web\ConarciFetch\Setup\ConarciFetchInstaller.exe"
+ 
+#downloads tvalue
+If(Test-Path $tvalue) {
+    Write-Host "T Value Found. Downloading..."
+    Start-Process -FilePath $tvalue -Wait
+    Write-Host "Setup Complete"
+} else {
+    Write-Host "Installer failed."
+}
 
+
+#downloads ichannel prereqs
+If(Test-Path $ichannelprereq) {
+    Write-Host "Ichannel Prereqs are downloading..."
+    Start-Process -FilePath $ichannelprereq -Wait
+    Write-Host "Setup is complete"
+} else {
+    Write-Host "Download failed" 
+}
+
+#Downloads ichannel
+If(Test-Path $ichannel) {
+    Write-Host "Ichannel Prereqs are downloading..."
+    Start-Process -FilePath $ichannel -Wait
+    Write-Host "Setup is complete"
+} else {
+    Write-Host "Download failed" 
+}
